8f98e7e75551e951fb99f15d802b1f9f127abab1
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 16:17:09 +0100

    clean-up after mounting

--- src/component/phone-number/phone-number.test.js
+++ src/component/phone-number/phone-number.test.js
@@ -5,10 +5,11 @@ import { render } from '@testing-library/react'
 
 describe('Phone number', () => {
   it('renders the component', () => {
-    const { getByLabelText } = render(<PhoneNumber />)
+    const { getByLabelText, unmount } = render(<PhoneNumber />)
     const input = getByLabelText(/Phone number/i)
-
+    
     expect(input.type).toBe('number')
     expect(input).toHaveAttribute('type', 'number')
+    unmount()
   })
 })
