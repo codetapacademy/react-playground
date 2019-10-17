cc3c2f3b33cf5b9438cf1ad0c700a84087163ddd
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 16:05:38 +0100

    refactor in a renderUserInterface fat arrow function

--- src/component/phone-number/phone-number.test.js
+++ src/component/phone-number/phone-number.test.js
@@ -4,13 +4,20 @@ import { PhoneNumber } from '.'
 import { getQueriesForElement } from '@testing-library/dom'
 import '@testing-library/jest-dom/extend-expect'
 
+const renderUserInterface = component => {
+  const box = document.createElement('div')
+  render(component, box)
+  const queries = getQueriesForElement(box)
+  return {
+    box,
+    ...queries,
+  }
+}
+
 describe('Phone number', () => {
   it('renders the component', () => {
-    const div = document.createElement('div')
-    render(<PhoneNumber />, div)
-    const { getByLabelText } = getQueriesForElement(div)
-
-    const input = getByLabelText(div, /Phone number/i)
+    const { getByLabelText } = renderUserInterface(<PhoneNumber />)
+    const input = getByLabelText(/Phone number/i)
 
     expect(input.type).toBe('number')
     expect(input).toHaveAttribute('type', 'number')
