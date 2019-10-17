a5b4470267ee82cc1f89abf97c797e0e4a2d5e5f
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 16:01:08 +0100

    use getQueriesForElement instead of queries

--- src/component/phone-number/phone-number.test.js
+++ src/component/phone-number/phone-number.test.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { PhoneNumber } from '.'
-import { queries } from '@testing-library/dom'
+import { getQueriesForElement } from '@testing-library/dom'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Phone number', () => {
   it('renders the component', () => {
     const div = document.createElement('div')
     render(<PhoneNumber />, div)
+    const { getByLabelText } = getQueriesForElement(div)
 
-    const input = queries.getByLabelText(div, /Phone number/i)
+    const input = getByLabelText(div, /Phone number/i)
 
     expect(input.type).toBe('number')
     expect(input).toHaveAttribute('type', 'number')
