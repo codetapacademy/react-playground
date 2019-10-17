81bac7fd03cb9885e0fe50d3c79cc90f4dadc3c7
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 15:42:11 +0100

    extend expect with toHaveTextContent

--- src/component/phone-number/phone-number.test.js
+++ src/component/phone-number/phone-number.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { PhoneNumber } from '.'
-import { toHaveAttribute } from '@testing-library/jest-dom'
+import { toHaveAttribute, toHaveTextContent } from '@testing-library/jest-dom'
 
-expect.extend({ toHaveAttribute })
+expect.extend({ toHaveAttribute, toHaveTextContent })
 
 describe('Phone number', () => {
   it('renders the component', () => {
@@ -11,6 +11,7 @@ describe('Phone number', () => {
     render(<PhoneNumber />, div)
 
     expect(div.querySelector('label').textContent).toBe('Phone number')
+    expect(div.querySelector('label')).toHaveTextContent('Phone number')
     expect(div.querySelector('input').type).toBe('number')
     expect(div.querySelector('input')).toHaveAttribute('type', 'number')
   })
