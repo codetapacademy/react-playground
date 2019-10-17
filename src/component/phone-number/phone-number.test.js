b4c34a72ecacfb2298e38e8b4276657382966b91
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 16:18:10 +0100

    replace unmount with clean-up

--- src/component/phone-number/phone-number.test.js
+++ src/component/phone-number/phone-number.test.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { PhoneNumber } from '.'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, cleanup } from '@testing-library/react'
 
 describe('Phone number', () => {
   it('renders the component', () => {
-    const { getByLabelText, unmount } = render(<PhoneNumber />)
+    const { getByLabelText } = render(<PhoneNumber />)
     const input = getByLabelText(/Phone number/i)
     
     expect(input.type).toBe('number')
     expect(input).toHaveAttribute('type', 'number')
-    unmount()
+    cleanup()
   })
 })
