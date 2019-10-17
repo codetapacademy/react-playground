0f4f444f70108d55326c1351de6a923bed240c03
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 15:52:51 +0100

    test link between label and input using queries from react testing libray dom

--- src/component/phone-number/phone-number.test.js
+++ src/component/phone-number/phone-number.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { PhoneNumber } from '.'
+import { queries } from '@testing-library/dom'
 import '@testing-library/jest-dom/extend-expect'
 
 describe('Phone number', () => {
@@ -8,9 +9,11 @@ describe('Phone number', () => {
     const div = document.createElement('div')
     render(<PhoneNumber />, div)
 
+    const input = queries.getByLabelText(div, 'Phone number')
+
     expect(div.querySelector('label').textContent).toBe('Phone number')
     expect(div.querySelector('label')).toHaveTextContent('Phone number')
-    expect(div.querySelector('input').type).toBe('number')
-    expect(div.querySelector('input')).toHaveAttribute('type', 'number')
+    expect(input.type).toBe('number')
+    expect(input).toHaveAttribute('type', 'number')
   })
 })
