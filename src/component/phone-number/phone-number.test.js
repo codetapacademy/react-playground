8eb2bc49400a4b1b9f4494fac2dba93c8257dfd1
Marian Zburlea <marianzburlea@gmail.com>
2019-10-17 15:40:34 +0100

    extend expect

--- src/component/phone-number/phone-number.test.js
+++ src/component/phone-number/phone-number.test.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { PhoneNumber } from '.'
+import { toHaveAttribute } from '@testing-library/jest-dom'
+
+expect.extend({ toHaveAttribute })
 
 describe('Phone number', () => {
   it('renders the component', () => {
@@ -9,5 +12,6 @@ describe('Phone number', () => {
 
     expect(div.querySelector('label').textContent).toBe('Phone number')
     expect(div.querySelector('input').type).toBe('number')
+    expect(div.querySelector('input')).toHaveAttribute('type', 'number')
   })
 })
